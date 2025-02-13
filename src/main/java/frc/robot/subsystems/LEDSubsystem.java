9e84bab54baf928dc80d0becb6f73d4c364cf88f
Boazmeisler <151450897+Boazmeisler@users.noreply.github.com>
2025-02-13 18:33:37 +0200

    ledplshelp

--- src/main/java/frc/robot/subsystems/LEDSubsystem.java
+++ src/main/java/frc/robot/subsystems/LEDSubsystem.java
@@ -23,7 +23,7 @@ public class LEDSubsystem extends SubsystemBase {
     // Set the default command to turn the strip off, otherwise the last colors written by
     // the last command to run will continue to be displayed.
     // Note: Other default patterns could be used instead!
-    setDefaultCommand(runPattern(LEDPattern.solid(Color.kBlack)).withName("Off"));
+    setDefaultCommand(runPattern(LEDPattern.solid(Color.kGreen)).withName("Off"));
   }
 
   @Override
